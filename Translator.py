bf8827aa856ecd60ee1f51c95ed0b2a642234706
NormanGadenya <ngadenya@gmail.com>
2020-06-24 06:18:47 +0300

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,19 @@
+#converts speech from english to french
+import speech_recognition as sr
+from googletrans import Translator
+from gtts import gTTS
+from playsound import playsound
+
+r=sr.Recognizer()
+with sr.Microphone() as source:
+    print('speak anything')
+    audio=r.listen(source)
+    text=r.recognize_google(audio)
+    print('you said: {}'.format(text))
+    
+translator=Translator()
+mytext=translator.translate(text,dest='fr').text
+
+myobj = gTTS(text=mytext, lang='fr', slow=False) 
+myobj.save("speech.mp3")
+playsound('speech.mp3')
